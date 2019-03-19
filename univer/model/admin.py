51f046555fea42b1655744a9b2d1baf4495a6d2f
erkanderon <erkan.cetinkaya3@gmail.com>
2019-03-19 22:33:11 +0300

    Teachers and University objects initialized

--- univer/model/admin.py
+++ univer/model/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-
+admin.site.register(University)
+admin.site.register(Teacher)
 admin.site.register(Course)
+
--- univer/model/models.py
+++ univer/model/models.py
@@ -2,7 +2,57 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-# === Models for Blog app ===
+
+
+class University(models.Model):
+	pop_status = (
+		(1, 'Half Star'),
+		(2, 'One Star'),
+		(3, 'One and Half Star'),
+		(4, 'Two Star'),
+		(5, 'Two and Half Star'),
+		(6, 'Three Star'),
+		(7, 'Three and Half Star'),
+		(8, 'Four Star'),
+		(9, 'Four and Half Star'),
+		(10, 'Five Star')
+	)
+
+	university_name = models.CharField(max_length=50)
+	university_description = models.TextField(null=True, blank=True)
+	university_logo = models.CharField(max_length=150)
+	university_popularity = models.IntegerField(choices=pop_status)
+
+	def __str__(self):
+		return self.university_name
+
+class Teacher(models.Model):
+	# Course teachers
+	pop_status = (
+		(1, 'Half Star'),
+		(2, 'One Star'),
+		(3, 'One and Half Star'),
+		(4, 'Two Star'),
+		(5, 'Two and Half Star'),
+		(6, 'Three Star'),
+		(7, 'Three and Half Star'),
+		(8, 'Four Star'),
+		(9, 'Four and Half Star'),
+		(10, 'Five Star')
+	)
+
+	teacher_title = models.CharField(max_length=20, null=True)
+	teacher_name = models.CharField(max_length=30)
+	teacher_surname = models.CharField(max_length=30)
+	teacher_description = models.TextField(blank=True, null=True)
+	teacher_image = models.CharField(max_length=150)
+	teacher_popularity = models.IntegerField(choices = pop_status)
+	teacher_university = models.OneToOneField(University, on_delete=models.SET_NULL, null=True)
+
+	def __str__(self):
+		return self.teacher_title + ' ' +self.teacher_name + ' ' +self.teacher_surname
+
+# === Models for Learning app ===
 
 class Course(models.Model):
 	"""
@@ -25,6 +75,8 @@ class Course(models.Model):
 	price_currency = models.CharField(max_length=5, choices=price_currencies)
 	pub_date = models.DateField(auto_now_add=True)
 	course_status = models.IntegerField(choices=course_status)
+	course_teacher = models.ManyToManyField(Teacher, blank=True)
+	course_logo = models.CharField(max_length=150, null=True)
 
 
 	'''def get_absolute_url(self):
@@ -32,3 +84,4 @@ class Course(models.Model):
 
 	def __str__(self):
 		return self.course_title
+
